f44bcab3e98329ea9cfac2cdd143e75abd05d94b
Teng YANG <teng.yang@oocl.com>
2019-08-12 19:18:06 +0800

    [GildedRose] Add test case for Sulfuras

--- src/test/java/gildedRose/GildedRoseTest.java
+++ src/test/java/gildedRose/GildedRoseTest.java
@@ -123,7 +123,7 @@ public class GildedRoseTest {
     }
 
     @Test
-    public void shouldReturn50_givenBackstageISellInIs0AndQualityIs20() {
+    public void shouldReturn0_givenBackstageISellInIs0AndQualityIs20() {
         Item item = new Item(BACKSTAGE_NAME, 0, 20);
         GildedRose gildedRose = new GildedRose(new Item[]{item});
 
@@ -132,4 +132,23 @@ public class GildedRoseTest {
         assertEquals(0, item.quality);
     }
 
+    @Test
+    public void shouldReturn80_givenSulfurasSellInIs10AndQualityIs80() {
+        Item item = new Item(SULFURAS_NAME, 10, 80);
+        GildedRose gildedRose = new GildedRose(new Item[]{item});
+
+        gildedRose.updateQuality();
+
+        assertEquals(80, item.quality);
+    }
+
+    @Test
+    public void shouldReturn80_givenSulfurasSellInIs0AndQualityIs80() {
+        Item item = new Item(SULFURAS_NAME, 0, 80);
+        GildedRose gildedRose = new GildedRose(new Item[]{item});
+
+        gildedRose.updateQuality();
+
+        assertEquals(80, item.quality);
+    }
 }
